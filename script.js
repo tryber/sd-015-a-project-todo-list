83ae26c198289787181d4c1ac9671877354630ea
Seu nome <mgabiss10@gmail.com>
2021-08-20 17:26:51 -0300

    Requisito 5 e 6

---
+++ script.js
@@ -0,0 +1,6 @@
+// Requisito 5 e 6-(Por ser uma ol, ja está ordenado os itens da lista por ordem de colocação)
+let createBtn = document.getElementById('criar-tarefa');
+let insertText = document.getElementById('texto-tarefa');
+let ol = document.querySelector('#lista-tarefas');
+let deleteBtn = document.getElementById ('apaga-tudo');
+
