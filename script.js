e04424a7410cc8aef38de9140510d7136d9211a5
Omar Merljak <omerljak@gmail.com>
2021-08-20 13:58:30 -0300

    requisito 8 com apoio em mentoria

--- script.js
+++ script.js
@@ -14,30 +14,32 @@ const eventoBotao = document.querySelector('#criar-tarefa');
 
 /* criar constantes de uso comum fora da função facilita pois podem ser usadas entre funções */
 function criaLi() {
+
   let criaLi = document.createElement('li');
   criaLi.className = 'line';
   criaLi.innerText = buscaTextoInput.value
   criaLi.addEventListener('click', corLi);
-  criaLi.addEventListener('dblclick', riscaLi)
+  //criaLi.addEventListener('dblclick', riscaLi)
+  /* criaLi.addEventListener('click', btnApaga) */
   buscaTextoInput.value = '';
   buscalistaTarefas.appendChild(criaLi);
 }
 eventoBotao.addEventListener('click', criaLi)
 
-function corLi(event) { 
-  event.target.classList.add('selected');
- /*  if (!target.classList('selected')) {
+function corLi(event) { //com apoio em mentoria com Ronald..a função chama com a let 'x' o elemento com a classe 'selected' e o if verifica se 'x' existe com a classe. Se existe é pedido que a classe seja removida ou adicionada pelo evento.
+  let x = document.querySelector('.selected');
+  if (x) {
+    x.classList.remove('selected');  
+  } 
     event.target.classList.add('selected');
-  } else {
-    event.target.classList.remove('selected');
-  }*/
 } 
 
-function riscaLi(dpclick) {
+/* function riscaLi(dpclick) {
   if (target.classList('selected'))
   dpclick.target.classList.remove('selected');
   dpclick.target.classList.add('completed');
-}
+} */
+
 
 /* 
 function eventosLi () {
