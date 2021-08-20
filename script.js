6ade0fdd4cd895408981cb2ecfa07cf181a80aaf
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-08-20 01:13:00 -0300

    refatora requisito 8

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ let addButton = document.querySelector('#criar-tarefa');
 let taskList = document.querySelector('#lista-tarefas');
 let inputArea = document.querySelector('#texto-tarefa');
 
-//Cria lista de tarefas
+//Cria li's na #lista-tarefas
 function addTask() {
   let listItem = document.createElement('li');
   taskList.appendChild(listItem);
   listItem.innerHTML = inputArea.value;
   listItem.classList = 'list-item';
   inputArea.value = '';
-  listItem.addEventListener('click', selectItem);
   listItem.addEventListener('click', changeItemColor);
 }
+
 addButton.addEventListener('click', addTask);
 
-//Altera cor de fundo dos itens da lista
+//Altera cor de fundo do item selecionado.
 function changeItemColor() {
-  this.style.backgroundColor = 'red';
-  if (document.querySelector('.selected')) {
+  if (document.querySelector('.selected')) { //Checa se existe algum elemento com a classe selected.
+    //Seleciona o elemento selected, remove sua cor de fundo e sua classe selected.
     let selectedColor = document.querySelector('.selected');
+    selectedColor.style.backgroundColor = 'transparent';
     selectedColor.classList.remove('selected');
   }
-  this.classList = 'selected';
+  //Elemento de origem do evento recebe a classe selected e a cor de fundo especial.
+  this.classList = 'selected'; 
+  let selectedColor = document.querySelector('.selected');
+  selectedColor.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
-function selectItem() {
-  let selectedColor = document.querySelector('.selected');
-  selectedColor.classList.remove('selected');
-}
 
