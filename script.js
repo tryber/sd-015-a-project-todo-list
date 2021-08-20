dec8fcb0260e4c6945226071a7f08c673b004b10
Davio <davioplp@gmail.com>
2021-08-20 02:38:22 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const list = document.querySelector('#lista-tarefas');
+function risc(riscado) {
+  const allList4 = document.querySelectorAll('li');
+  const itemRiscado = riscado.target;
+  if (itemRiscado.className === 'completed') {
+    itemRiscado.removeAttribute('class');
+  } else {
+    itemRiscado.classList.add('completed');
+  }
+}
+
+function evento2() {
+  const allList3 = document.querySelectorAll('li');
+  for (let i = 0; i < allList3.length; i += 1) {
+    allList3[i].addEventListener('dblclick', risc);
+  }
+}
+
 function paintGrey(item) {
   const allList2 = document.querySelectorAll('li');
   for (let i = 0; i < allList2.length; i += 1) {
     allList2[i].style.removeProperty('background-color');
   }
-
   const itemGrey = item.target;
   itemGrey.style.backgroundColor = 'grey';
 }
@@ -24,6 +40,8 @@ function insertElement() {
   const input = document.getElementById('texto-tarefa');
   input.value = '';
   evento();
+  evento2();
 }
 
 document.getElementById('criar-tarefa').addEventListener('click', insertElement);
+document.getElementById('funcionamento').addEventListener('dblclick', risc);
