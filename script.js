1cecc5c9f487dce7dfbaade0a10a6ff6742ed46a
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-08-20 00:07:32 -0300

    adiciona parte do requisito 8

--- script.js
+++ script.js
@@ -9,16 +9,23 @@ function addTask() {
   listItem.innerHTML = inputArea.value;
   listItem.classList = 'list-item';
   inputArea.value = '';
+  listItem.addEventListener('click', selectItem);
   listItem.addEventListener('click', changeItemColor);
 }
 addButton.addEventListener('click', addTask);
 
 //Altera cor de fundo dos itens da lista
 function changeItemColor() {
-  let listItens = document.querySelectorAll('.list-item');
-  for (let index of listItens) {
-    this.style.backgroundColor = 'rgb(128, 128, 128)';
+  this.style.backgroundColor = 'red';
+  if (document.querySelector('.selected')) {
+    let selectedColor = document.querySelector('.selected');
+    selectedColor.classList.remove('selected');
   }
-  console.log(listItens);
+  this.classList = 'selected';
+}
+
+function selectItem() {
+  let selectedColor = document.querySelector('.selected');
+  selectedColor.classList.remove('selected');
 }
 
