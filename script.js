f553dd876adba48af04a65047ad5a2b88d7effc9
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-08-20 11:41:21 -0300

    tentando resolver erros.

--- script.js
+++ script.js
@@ -30,12 +30,10 @@ CriarBotaoAddTarefa();
 function mudarACorDeFundo(event) {
   const evento = event.target;
   const selection = document.querySelector('.selected');
-  if (selection) selection.classList.remove('selected');
-  if (evento.classList.contains('selected')) {
-    evento.classList.remove('selected');
-  } else {
-    evento.classList.add('selected');
+  if (selection) {
+    selection.classList.remove('selected');
   }
+    evento.classList.add('selected');
 }
 
 function addOuRemoverClasseCompleted(event) {
