256e0b05d0786c9025d1d76a8b59fb2434972d72
carla <carlabazevedoa@gmail.com>
2021-08-21 16:23:48 -0300

    caminhandoo

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-document.getElementById('criar-tarefa').addEventListener('click', function () {
+document.getElementById('criar-tarefa').addEventListener('click', function (event) {
+  event.preventDefault();
   const usersText = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
   li.textContent = usersText;
   document.getElementById('lista-tarefas').appendChild(li);
-  document.getElementById('texto-tarefa').value = "";
+  document.getElementById('texto-tarefa').value = '';
+  li.addEventListener('click', function (event2) {
+  event2.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
 });
-
-
