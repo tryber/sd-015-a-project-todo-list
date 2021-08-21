9d6ffd6c269a343c3fd80d2c08cb6062574190cd
Lucas Macedo <lucasrrm22@gmail.com>
2021-08-21 17:02:26 -0300

    requisito 9

--- script.js
+++ script.js
@@ -5,29 +5,27 @@ const listOfTasks = document.querySelector('#lista-tarefas')
 button.addEventListener('click', addTask)
 function addTask() {
     let task = document.createElement('li');
-    task.className = 'notSelected'
     task.innerHTML = input.value;
     input.value = '';
     listOfTasks.appendChild(task);
+    task.addEventListener('dblclick', done)
 }
 listOfTasks.addEventListener('click', greyColor);
 function greyColor(event) {
-    let notSelected = document.querySelector('.notSelected')
-    let white = getComputedStyle(notSelected)
     let allTask = document.getElementsByTagName('li')
-    for (i = 0; i < allTask.length; i +=1) {
-        allTask[i].className = 'notSelected'
+    if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected')
     }
-    event.target.className = 'selected'
-    let selected = document.querySelector('.selected')
-    let style = getComputedStyle(selected)
-    for (let l = 0; l < allTask; l += 1) {
-        if (allTask[l].className == 'notSelected') {
-            allTask[l].style.backgroundColor = 'white'
-        } else {
-            allTask[l].style.backgroundColor = style.backgroundColor
-        }
-    }
+    event.target.classList.add('selected')
     
+    
+}
+function done(event) {
+    if (event.target.classList.contains('completed')) {
+        document.querySelector('.completed').classList.remove('completed');
+    } else {
+    event.target.classList.add('completed');
+    }
+  
 }
 }
