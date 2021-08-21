3e3382f3c872f54d377c1a10d35242ba2a05b0f9
carla <carlabazevedoa@gmail.com>
2021-08-21 20:54:48 -0300

    add clique e db clique

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ document.getElementById('criar-tarefa').addEventListener('click', function (even
   li.textContent = usersText;
   document.getElementById('lista-tarefas').appendChild(li);
   document.getElementById('texto-tarefa').value = '';
-  li.addEventListener('click', function (event2) {
-  event2.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  });
+  li.addEventListener('click', setBackgroundColor);
+  li.addEventListener('dblclick', setCompleted);
 });
+
+//pra add cor ao novo item clicado e tirando do anterior. 
+//array.forEach(function (element){}) ~ for (let element of array) {}
+
+function setBackgroundColor(event) {
+  document.querySelectorAll('.selected').forEach(function(li) {
+    li.classList.remove('selected');
+  });
+  event.target.classList.add('selected');
+};
+
+//add risco e remover risco de item completo.
+function setCompleted(event) {
+  const li = event.target;
+  if (li.classList.contains('completed')) {
+    li.classList.remove('completed');
+  } else {
+    li.classList.add('completed');
+  }
+};
