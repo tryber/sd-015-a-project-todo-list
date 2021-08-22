514314a26652d30b51a7c7b5fee2c278233fba9a
Leandro Basilio <leandroab07@gmail.com>
2021-08-22 20:20:01 -0300

    Adiciona funcionalidade para salvar as tarefas adicionadas, ainda que a página seja recarregada

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnAdicionar = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const btnApagar = document.getElementById('apaga-tudo');
 const btnRemover = document.getElementById('remover-finalizados');
+const btnSalvarDados = document.getElementById('salvar-tarefas');
 
 // Adiciona cor de fundo em apenas um elemento da lista
 // Recebi suporte do Matheus Monteiro e do Diego Brito
@@ -56,8 +57,25 @@ function apagaConcluidas() {
   }
 }
 
+// Adiciona os dados a serem salvos no localstorage
+// Para essa função, eu olhei o commit do Italo Sérgio, da turma 15 da Trybe, para ter uma ideia de como fazer
+function salvaDados() {
+  let dadosSalvos = document.getElementById('lista-tarefas').innerHTML;
+  localStorage.setItem('salvar', dadosSalvos);
+}
+
+// Função para manter os dados ao recarregar a página usando o localStorage
+// Para essa função, eu olhei o commit do Italo Sérgio, da turma 15 da Trybe, para ter uma ideia de como fazer
+function salvados() {
+  lista.innerHTML = localStorage.getItem('salvar');
+}
+
 btnAdicionar.addEventListener('click', adicionaTarefa);
 
 btnApagar.addEventListener('click', apagaTarefas);
 
-btnRemover.addEventListener('click', apagaConcluidas)
+btnRemover.addEventListener('click', apagaConcluidas);
+
+btnSalvarDados.addEventListener('click', salvaDados);
+
+window.onload = salvados;
