7da71b6cae67eac7aeafb07980fd769ac66cf278
carla <carlabazevedoa@gmail.com>
2021-08-21 21:10:13 -0300

    ajuste de lint

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-document.getElementById('criar-tarefa').addEventListener('click', function (event) {
+// pra add cor ao novo item clicado e tirando do anterior.
+// array.forEach(function (element){}) ~ for (let element of array) {}
+
+function setBackgroundColor(event) {
+  document.querySelectorAll('.selected').forEach((li) => {
+    li.classList.remove('selected');
+  });
+  event.target.classList.add('selected');
+}
+
+// add risco e remover risco de item completo.
+
+function setCompleted(event) {
+  const li = event.target;
+  if (li.classList.contains('completed')) {
+    li.classList.remove('completed');
+  } else {
+    li.classList.add('completed');
+  }
+}
+
+document.getElementById('criar-tarefa').addEventListener('click', (event) => {
   event.preventDefault();
   const usersText = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
@@ -8,23 +29,3 @@ document.getElementById('criar-tarefa').addEventListener('click', function (even
   li.addEventListener('click', setBackgroundColor);
   li.addEventListener('dblclick', setCompleted);
 });
-
-//pra add cor ao novo item clicado e tirando do anterior. 
-//array.forEach(function (element){}) ~ for (let element of array) {}
-
-function setBackgroundColor(event) {
-  document.querySelectorAll('.selected').forEach(function(li) {
-    li.classList.remove('selected');
-  });
-  event.target.classList.add('selected');
-};
-
-//add risco e remover risco de item completo.
-function setCompleted(event) {
-  const li = event.target;
-  if (li.classList.contains('completed')) {
-    li.classList.remove('completed');
-  } else {
-    li.classList.add('completed');
-  }
-};
