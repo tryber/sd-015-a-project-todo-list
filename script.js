c99ea0f6359e8f4fe998f93d3f9608a84228c957
Douglas <douglascoelho31@hotmail.com>
2021-08-22 12:19:29 -0300

    requisito7 ok

--- script.js
+++ script.js
@@ -2,21 +2,14 @@ const botao = document.getElementById('criar-tarefa');
 const inputa = document.getElementById('texto-tarefa');
 const listaOrdenada = document.getElementById('lista-tarefas');
 
-
-function botaoAdiciona(){
-const novaLista = document.createElement('li');
-listaOrdenada.appendChild(novaLista);
-novaLista.innerText = inputa.value;
-inputa.value = '';
+function botaoAdiciona() {
+  const novaLista = document.createElement('li');
+  listaOrdenada.appendChild(novaLista);
+  novaLista.innerText = inputa.value;
+  inputa.value = '';
+  listaOrdenada.addEventListener('click', colorAdiciona)
 }
 botao.addEventListener('click', botaoAdiciona);
-
-
-/*5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
-O que será verificado:
-
-Será verificada a existência de um elemento do tipo button com o id criar-tarefa
-
-No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
-
-A adição de elementos na lista será feita algumas vezes, e será checado se todos os itens criados permanecem na lista na medida em que novos são adicionados.*/
+function colorAdiciona(cor) {
+  cor.target.style.backgroundColor = 'rgb(128,128,128)';
+}
