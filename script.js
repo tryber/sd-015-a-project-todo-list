4f0041b81cdc97cb98f82df80fd45fcad2f1a4d3
MoisésFigueira <moisesmichael@gmail.com>
2021-08-22 10:48:06 -0300

    Exercise 7

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-let button = document.getElementById('criar-tarefa');
+const button = document.getElementById('criar-tarefa');
 
-function addList(e) {
-   let input = document.getElementById('texto-tarefa').value;
-   document.getElementById('lista-tarefas').innerHTML += `<li>${input}</li>`;
-   document.getElementById('texto-tarefa').value = '';
-   document.getElementById('texto-tarefa').focus();
+function addList() {
+  const input = document.getElementById('texto-tarefa').value;
+  document.getElementById('lista-tarefas').innerHTML += `<li class='list'>${input}</li>`;
+  document.getElementById('texto-tarefa').value = '';
+  document.getElementById('texto-tarefa').focus();
 }
 
-button.addEventListener('click', addList)
+button.addEventListener('click', addList);
+
+const ol = document.getElementById('lista-tarefas');
+
+function color(e) {
+  let li = e.target;
+  li.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+function listSelect() {
+  const selectList = document.querySelectorAll('.list');
+let list;  
+for (list of selectList) {
+  list.addEventListener('click', color);
+}
+}
+
+const body = document.querySelector('body');
+
+body.addEventListener('click', listSelect)
