23af3c33c4e21719415f869110283f8dd156fdce
antoniofortisneto <antoniofortis@gmail.com>
2021-08-23 08:54:18 -0300

    caramba,virei a noite

---
+++ script.js
@@ -0,0 +1,15 @@
+addTarefa = document.getElementById("criar-tarefa");// curiosidade pra nao esquecer, o getelemente nao usa o # e o queryselector usa, essa é a diferença eu acho
+listaTarefa = document.querySelector('#lista-tarefa');// mas da pra usar o queryselector com o # na duvida
+caixaTexto = document.getElementById("texto-tarefa");
+
+function adicionador(){    
+const itemListado = document.createElement("p") ;
+itemListado.innerHTML = caixaTexto.value;
+document.querySelector("listaTarefa").appendChild(itemListado);
+caixaTexto.value= "trigo";
+}
+
+addTarefa.addEventListener("click", adicionador);
+document.addEventListener("DOMContentLoaded", adicionador);// Pode usar DOMContentLoaded para checar se o DOM já carregou
+
+
