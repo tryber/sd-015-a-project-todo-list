d1ebdcf8bf915b1e01faae7511e4c90a55665f26
Valdeci <moura97.valdeci@gmail.com>
2021-08-23 20:24:32 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ let list = document.querySelector('#lista-tarefas');
 
 button.addEventListener('click', function(){
   let listItem = document.createElement('li');
+  listItem.classList.add('task');
   listItem.innerHTML = input.value;
   list.appendChild(listItem);
   input.value = '';
-})
+});
+
+function greyColor(event){
+  let li = document.querySelectorAll('li');
+  for (let i = 0; i < li.length; i += 1){
+    if (li[i].style.backgroundColor = 'rgb(128, 128, 128)'){
+      li[i].style.backgroundColor = 'white';
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  }
+}
+
+list.addEventListener('click', greyColor);
