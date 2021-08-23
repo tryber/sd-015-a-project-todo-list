d9cd78eb807b9fd9c1ee3288b235c2014279e769
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-08-23 20:09:42 -0300

    requito do 8 ao 10

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
-window.onload = function (){
+window.onload = function(){
 
-    function TaskList(){
-    const textTask = document.querySelector('#texto-tarefa')
-    const listTask = document.querySelector('#lista-tarefas')
-    const createTask = document.querySelector('#criar-tarefa')
+    const taskButton = document.querySelector('#criar-tarefa')
+    const ol = document.querySelector('ol')
+    const textInput = document.querySelector('#texto-tarefa')
 
-    createTask.addEventListener('click', function (){
+    taskButton.addEventListener('click', function(){
       let list = document.createElement('li')
-      listTask.appendChild(list)
-      list.innerText = textTask.value
-      textTask.value = "";
+      list.classList.add("elemento");
+      ol.appendChild(list)
+      list.innerText = textInput.value
+      textInput.value = "";
+    })
+ 
+    ol.addEventListener('click', function(event){   
+      const selectClass = document.querySelector('.selected')       
+        if (selectClass) {
+           
+         
+          selectClass.classList.remove('selected');
+          selectClass.style.backgroundColor = ''
+          
+         }event.target.classList.add('selected');
+          event.target.style.backgroundColor = 'rgb(128,128,128)'; 
+        }) 
 
-      list.addEventListener ('click', function (event){
-          let clickedLi = event.target
-          clickedLi.style.backgroundColor = 'rgb(128, 128, 128'  
-      })
-      
-    }) 
 
-}TaskList()
-}
+      ol.addEventListener('dblclick', throughLine);
+
+        function throughLine(event) {
+          if (event.target.classList.contains('completed')){
+            event.target.classList.remove('completed')         
+        }  else {
+            event.target.classList.add('completed') 
+        }
+        }
+        const deleteAllButton = document.querySelector("#apaga-tudo")
+        deleteAllButton.addEventListener('click', deleteAllTask);
+
+        function deleteAllTask() {
+            const li = document.querySelectorAll('li')
+            for (let i = 0; i < li.length; i +=1){
+            li[i].remove();
+            }
+          }
+
+        // ol.addEventListener('dblclick',throughLine) 
+        // function throughLine(event){
+        //   console.log(event.target.classList.contains('completed'))
+        //   if (event.target.classList.contains('completed') === true){
+        //     event.target.classList.add('completed')
+
+        //   }else {
+        //   event.target.classList.remove('completed');
+
+        //   }
+        
+        // } 
+      }
+
+
