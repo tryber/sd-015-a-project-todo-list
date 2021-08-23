cd22a8cb6266dacc3a5f6a9ec6d79e79d18e3431
antoniofortisneto <antoniofortis@gmail.com>
2021-08-23 14:22:51 -0300

    ta quase agora

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = () => { // por algum motivo sem isso nao funciona
     addTarefa.addEventListener("click",adicionador);
     
     function adicionador(){    
-    let it = document.createElement("ol") ;
+    let it = document.createElement("li") ;
     it.innerHTML = caixaTexto.value;
     document.querySelector('#lista-tarefas').appendChild(it);
     caixaTexto.value = '';
