3ec3a36f046759b5a49b9db9bb6ff049ff37fecb
Elias Paiva <elias.jpaiva@gmail.com>
2021-08-23 17:43:09 +0200

    requesito 1

---
+++ script.js
@@ -0,0 +1,9 @@
+// Requesito 1
+let selectBody = document.querySelector('body'); // selecionar tag body
+let creatHeader = document.createElement('header'); // criar tag header
+creatHeader.innerHTML = "Minha Lista de Tarefas"; // adicionar texto header
+selectBody.appendChild(creatHeader); // colocar header como filho do body
+
+
+// Requesito 2
+
