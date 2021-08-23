e3072633871a1b70c8eacf76df562a66fd64b9de
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2021-08-23 10:30:14 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -8,18 +8,36 @@ function addTask() {
   taskNew.innerHTML = inputText.value;
   listTasks.appendChild(taskNew);
   inputText.value = '';
-  
-
 }
+
+
+//Requisito 7
+
+function ClickColor() {
+  this.style.backgroundColor = 'rgb(128, 128, 128)';
+  console.log('click');
+}
+
 typeButton.addEventListener('click', addTask)
 
-//Pintando a lista
+//Requisito 8
 
-function ClickColor() {   
-  this.style.backgroundColor = 'rgb(128, 128, 128)';
-  console.log(this);
+
+function clickOneColor() {
+  const listItem = listTasks.children;
+    for (let i = 0; i < listItem.length; i += 1) {
+    listItem[i].style.backgroundColor = 'floralwhite';
+  }
 }
 
+//Segunda função criada com base no racicínio de Leonardo Felix.
+
+listTasks.addEventListener('click', (task) => {
+  clickOneColor();
+  const clickedTask = task;
+  clickedTask.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  console.log('pinta uma só')
+});
 
 // alert('Você chegou na sua lista de tarefas! Cumpra todo o combinado, ok? =D')
 // console.log("kk")
