7689cedad5d974e30252d644330ecb2d31dbf6d8
Jonas Candia <jonas57.jc@gmail.com>
2021-08-23 21:08:44 -0300

    requisito 7, 8 e 9

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const criarTarefa = document.getElementById('criar-tarefa');
-let texto = document.getElementById('texto-tarefa');
+const texto = document.getElementById('texto-tarefa');
 const listaTarefa = document.getElementById('lista-tarefas');
 
+// consultado o repositorio https://github.com/tryber/sd-015-a-project-todo-list/blob/arthur-teixeira-santos-todo-list-project/script.js
+// Alterar a cor de fundo
+
+function trocaCor(event) {
+  if (document.querySelector('.selecionada')) {
+    document.querySelector('.selecionada').classList.remove('selecionada');
+  }
+  event.target.classList.add('selecionada');
+}
+
+// Colocar como completa
+
+function completar(event) {
+  if (event.target.className.includes('completed')) {
+    document.querySelector('.completed').classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+// Criar evento
 function criTar(event) {
   event.preventDefault();
-  let criarLi = document.createElement('li');
+  const criarLi = document.createElement('li');
   criarLi.innerText = texto.value;
   listaTarefa.appendChild(criarLi);
+  criarLi.addEventListener('click', trocaCor);
+  criarLi.addEventListener('dblclick', completar);
   texto.value = '';
 }
+
 criarTarefa.addEventListener('click', criTar);
