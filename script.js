122912f3fbe74c927e20d9dc278c5381652675ed
GTmudin <testederealismo@gmail.com>
2021-08-24 13:39:59 -0300

    requisito 8

--- script.js
+++ script.js
@@ -3,49 +3,40 @@ let TvalorInput = document.querySelector('#texto-tarefa');
 let Tbutton = document.querySelector('#criar-tarefa');
 let Tlist = document.querySelector('#lista-tarefas');
 
+
+//criação das minhas 'li's
 function add(){
     const newAdd = document.createElement('li');
     newAdd.innerText = TvalorInput.value;
-    newAdd.classList.add('lista');
     Tlist.appendChild(newAdd);
     TvalorInput.value = '';
-    
+
     //newAdd.addEventListener('click',function(event){
         //if(event.target){
             //let lista = document.querySelectorAll('li');
 //background-color
-    function tugo(){
-        newAdd.classList.toggle('done')
-    }
-    newAdd.addEventListener('click', tugo)
+    //function tugo(event){
+    //  event.target.classList.add('done');
 
+//clique background cinza, único, ou seja apenas 1 elemento deve ter a classe 'done', enquanto o outro volta ao background normal
+    function clickcinza(event){
+        let lis = document.querySelectorAll('li');
+        for(let li of lis){
+        li.classList.remove('done');
+    }
+        event.target.classList.add('done');
+        
+    }
+
+    newAdd.addEventListener('click', clickcinza);
+
+//duplo clique line through
     function tugobarra(){
         newAdd.classList.toggle('completed')
     }
-    newAdd.addEventListener('dblclick', tugobarra)
+
+    newAdd.addEventListener('dblclick', tugobarra);
+
 }
 
 Tbutton.addEventListener('click', add );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function clickrgb(event){
-//    const listaback = document.querySelectorAll('lista');
-//    listaback.classList.remove('selected');
-//    event.target.classList.add('selected');
-
-//}
-
