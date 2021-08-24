ee95306f6e20a38b1313721324557771368c6e86
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-23 21:11:44 -0300

    Arrumando Identação

---
+++ script.js
@@ -0,0 +1,11 @@
+// let input = document.getElementById("texto-tarefa");
+// let ol = document.getElementById("criar-tarefas");
+// let button =document.getElementById("criar-tarefa");
+
+// button.addEventListener("click", function() {
+// let li = document.creatElement('li');
+// let task = input.value;
+// li.innerText = task;
+// ol.appendChild(li);
+// task=" ";
+// }
