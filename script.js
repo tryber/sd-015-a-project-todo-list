e8e356a7d0a50754ed9f08d39ac602181faa15d1
Pedro Henrique <pedrohrds05@hotmail.com>
2021-08-24 12:18:16 -0300

    Adicionando comentarios no codigo js

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-const accessButton = document.querySelector('#criar-tarefa');
-const accessInput = document.querySelector('#texto-tarefa');
-const accessDelete = document.querySelector('#apaga-tudo');
-const buttonFinished = document.querySelector('#remover-finalizados')
-
+//Add a line-through on the double-clicked object
 function completedTask(event) {
-  if (event.target.classList.length == 1) {
+  if (event.target.classList.length === 1) {
     event.target.classList.add('completed');
   } else {
     event.target.classList.remove('completed');
   }
 }
 
+// Independent function that serves to make all the objects in the list with a white background color
 function clearCompletedTask() {
   const accessLi = document.querySelectorAll('.list');
-  for(let i = 0; i < accessLi.length; i += 1) {
-    accessLi[i].style.backgroundColor = '';
+  for (let i = 0; i < accessLi.length; i += 1) {
+    accessLi[i].style.backgroundColor = 'white';
   }
 }
 
+// Function that assigns gray color to selected list element, but first calls clearCompletedTask function
 function selected(event) {
   clearCompletedTask();
   event.target.style.backgroundColor = 'gray';
 }
 
+
+// Function that adds input text to "li"
 function addText() {
+  const accessInput = document.querySelector('#texto-tarefa');
   const accessOl = document.querySelector('#lista-tarefas');
   const createLi = document.createElement('li');
-  createLi.innerText = accessInput.value;
+  createLi.innerText = accessInput.value; // Getting the input value
   createLi.classList.add('list');
   accessOl.appendChild(createLi);
-  accessInput.value = '';
+  accessInput.value = ''; // Resetting the input
   createLi.addEventListener('dblclick', completedTask);
   createLi.addEventListener('click', selected);
 }
 
+const accessButton = document.querySelector('#criar-tarefa');
 accessButton.addEventListener('click', addText);
 
+// Function that deletes all objects from the list
 function deleteList() {
   const accessOl = document.querySelector('#lista-tarefas');
   accessOl.innerText = '';
 }
 
+const accessDelete = document.querySelector('#apaga-tudo');
 accessDelete.addEventListener('click', deleteList);
 
+// Function that removes objects from the list that have a line-through
 function removeFinished() {
   const accessLi = document.querySelectorAll('.list');
   for (let i = 0; i < accessLi.length; i += 1) {
@@ -54,4 +59,5 @@ function removeFinished() {
   }
 }
 
+const buttonFinished = document.querySelector('#remover-finalizados');
 buttonFinished.addEventListener('click', removeFinished);
