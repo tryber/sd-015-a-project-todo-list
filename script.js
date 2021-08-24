51f387f9d129a142f99b410dfdc8efc0ca7a378a
Vitor <tutornews2@gmail.com>
2021-08-24 12:00:31 -0300

    Adiciona requesito 8.

--- script.js
+++ script.js
@@ -12,23 +12,40 @@ function addValue () {
     acessaLista.appendChild(criaLi);
     acessaInput.value = "";
       //backgroud-color
-    for(i=0;i<acessaLista.childNodes.length;i++){
-      let pe = document.querySelectorAll(".li")[i];
-      pe.addEventListener("click", clickc);
-      function clickc(){
-          //Sorce: https://stackoverflow.com/questions/14323082/why-doesnt-backgroundcolor-rgba-b-c-work -- erro de não adicionar a cor rgb is not defined , falta do "";
-        pe.style.backgroundColor ='rgb(128, 128, 128)';
-      } 
-    }
+        //Repositorio do Marcelo Adriano consultado para fazer essa parte  - https://github.com/tryber/sd-015-a-project-todo-list/pull/1/files
+      criaLi.addEventListener("click",addcolor)
+      function addcolor(evento) {
+        if (document.querySelector('.selected')) {
+          document.querySelector('.selected').classList.remove('selected');
+        }
+        evento.target.classList.add('selected');
+      }
   }
 
 const acessaButtomClear = document.getElementById("apaga-tudo");
 acessaButtomClear.addEventListener("click",clear);
-  //Repositorio do Arthur Teixeira Santos consultado para fazer essa parte Marcelo Adriano - https://github.com/tryber/sd-015-a-project-todo-list/pull/1/files
+  //Repositorio do Marcelo Adriano consultado para fazer essa parte  - https://github.com/tryber/sd-015-a-project-todo-list/pull/1/files
 function clear() {
   const acessaFilhos = acessaLista.children;
       //percorre enquanto tiver filho e remove o indice 0(toda vez que passa tem um filho a menos por isso não acrescenta).
   for (let index = 0; index < acessaFilhos.length; index+=0) {
       acessaLista.removeChild(acessaFilhos[0]);
     }
+}
+
+
+
+
+
+
+
+for(i=0;i<acessaLista.childNodes.length;i++){
+  let pe = document.querySelectorAll(".li")[i];
+  pe.addEventListener("click", clickc);
+  function clickc(){
+      //Sorce: https://stackoverflow.com/questions/14323082/why-doesnt-backgroundcolor-rgba-b-c-work -- erro de não adicionar a cor rgb is not defined , falta do "";
+    pe.className = "li selected";
+    const cor = document.querySelector(".selected");
+    cor.style.backgroundColor ='rgb(128, 128, 128)';
+  } 
 }
