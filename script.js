1d4cb5bff259379a96bbb0f54dabc7e96cd6ecc6
Vitor <tutornews2@gmail.com>
2021-08-24 10:53:56 -0300

    Adiciona requesito 10.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const acessaLista = document.getElementById("lista-tarefas");
 const acessaInput = document.getElementById("texto-tarefa");
-const acessaButtom = document.getElementById("criar-tarefa");
-  //Repositorio do Arthur Teixeira Santos consultado para fazer essa parte (addValue sendo colocado de forma errada e form.)
-acessaButtom.addEventListener("click", addValue);
+const acessaButtomAdd = document.getElementById("criar-tarefa");
+  //Repositorio do Arthur Teixeira Santos consultado para fazer essa parte (addValue sendo colocado de forma errada e form.) - https://github.com/tryber/sd-015-a-project-todo-list/pull/132/commits/37a8ec47effa00ca2bcae26904c31898f77352fe
+acessaButtomAdd.addEventListener("click", addValue);
 
 function addValue () {
     const criaLi = document.createElement("li");
@@ -14,11 +14,21 @@ function addValue () {
       //backgroud-color
     for(i=0;i<acessaLista.childNodes.length;i++){
       let pe = document.querySelectorAll(".li")[i];
-      pe.addEventListener('click', clickc);
+      pe.addEventListener("click", clickc);
       function clickc(){
           //Sorce: https://stackoverflow.com/questions/14323082/why-doesnt-backgroundcolor-rgba-b-c-work -- erro de não adicionar a cor rgb is not defined , falta do "";
         pe.style.backgroundColor ='rgb(128, 128, 128)';
       } 
     }
-}
+  }
 
+const acessaButtomClear = document.getElementById("apaga-tudo");
+acessaButtomClear.addEventListener("click",clear);
+  //Repositorio do Arthur Teixeira Santos consultado para fazer essa parte Marcelo Adriano - https://github.com/tryber/sd-015-a-project-todo-list/pull/1/files
+function clear() {
+  const acessaFilhos = acessaLista.children;
+      //percorre enquanto tiver filho e remove o indice 0(toda vez que passa tem um filho a menos por isso não acrescenta).
+  for (let index = 0; index < acessaFilhos.length; index+=0) {
+      acessaLista.removeChild(acessaFilhos[0]);
+    }
+}
