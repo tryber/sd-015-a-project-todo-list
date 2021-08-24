8b645783950a6efb4d9655586e8de099b415a47f
Valdeci <moura97.valdeci@gmail.com>
2021-08-23 23:30:21 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let input = document.querySelector('#texto-tarefa');
 let button = document.querySelector('#criar-tarefa');
 let list = document.querySelector('#lista-tarefas');
+let deleteButton = document.querySelector('#apaga-tudo');
 
 button.addEventListener('click', function(){
   let listItem = document.createElement('li');
-  listItem.classList.add('task');
+  //listItem.classList.add('task');
   listItem.innerHTML = input.value;
   list.appendChild(listItem);
   input.value = '';
@@ -21,3 +22,23 @@ function greyColor(event){
 }
 
 list.addEventListener('click', greyColor);
+
+function lineThrough(event){
+  let liDecoration = document.querySelectorAll('li');
+  for (let i = 0; i < liDecoration.length; i += 1){
+    if (event.target.classList !== 'completed'){
+      event.target.classList.add('completed');
+    } else {
+      event.target.classList.remove('completed');
+    }
+  }
+}
+
+list.addEventListener('dblclick', lineThrough);
+
+deleteButton.addEventListener('click', function(){
+  let li = document.querySelectorAll('li');
+  for (let i = 0; i < li.length; i += 1){
+    list.removeChild(li[i]);
+  }
+})
