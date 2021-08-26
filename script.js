b3fff69c42bf39853c0e98df3fa0b45bd9d9b8c0
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-25 22:20:03 -0300

    Formatação Prettier

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("criar-tarefa")
+
+function criarTarefa() {
+  let input = document.querySelector("#texto-tarefa");
+
+}
+button.addEventListener("click", criarTarefa)
+
+
