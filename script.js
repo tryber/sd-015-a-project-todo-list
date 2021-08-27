e924785240494c9bd8c766482be94012fed1191b
guilhermeadelino <guilhermeadelino@live.com>
2021-08-27 14:35:44 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ function addNovaTarefa() {
     tarefa.value = '';
 }
 
+const listaTarefa = document.getElementById('lista-tarefas');
 const botaoTarefa = document.getElementById('criar-tarefa');
 botaoTarefa.addEventListener('click', addNovaTarefa);
+
+function alteraCor(tarefas) {
+    const clickTarefa = tarefas.target;
+    clickTarefa.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+listaTarefa.addEventListener('click', alteraCor);
