5e44b0da4fbf4c62c324c5ff4ed067c1e5f41ff0
Rafael <grs.rafaelgomes@gmail.com>
2021-08-29 02:02:48 -0300

    adiciona Desafio 10

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-function criaTarefa (texto) {  const btn = document.querySelector("#criar-tarefa");
-
-btn.addEventListener("click", function(e){
-    let input = document.querySelector("#texto-tarefa");
-
-    let criaLista = document.createElement("li");
-
-    let lista = document.querySelector("#lista-tarefas");
-
-    criaLista.className = "tarefa"
-
-    criaLista.style.backgroundColor = "rgb(240, 240, 240)"
-    criaLista.innerText = input.value;
-
-    lista.appendChild(criaLista);
-    
-
-})
+const btnTarefa = document.querySelector('#criar-tarefa');
+const task = document.querySelector('#texto-tarefa');
+const list = document.getElementById('lista-tarefas');
+const selected = document.getElementsByClassName('selected');
+const removeTask = document.getElementById('apaga-tudo');
+const done = document.getElementsByClassName('completed');
+const btnRemove = document.getElementById('remover-finalizados');
+// const listChild = document.getElementById('lista-tarefas').children;
+function changClass(e) {
+  if (selected[0]) {
+    selected[0].classList.remove('selected');
+  }
+  e.target.classList.add('selected');
 }
 
-criaTarefa();
+function taskDone(t) {
+  const e = t.target;
+  if (e.className.includes('completed')) {
+    e.classList.remove('completed');
+  } else {
+    e.classList.add('completed');
+  }
+}
 
-function changeColor(){ 
-    let tarefas = document.querySelectorAll(".tarefa");
-    for(let i = 0; i<tarefas.length; i+=1){ 
-        tarefas[i].addEventListener("click",function(){if (tarefas[i].style.backgroundColor != "rgb(128, 128, 128)"){tarefas[i].style.backgroundColor = "rgb(128, 128, 128)"}
+function creatTask() {
+  btnTarefa.addEventListener('click', () => {
+    const creaTask = document.createElement('li');
+    creaTask.innerText = task.value;
+    creaTask.className = 'tasks';
+    creaTask.addEventListener('click', changClass);
+    creaTask.addEventListener('dblclick', taskDone);
+    list.appendChild(creaTask);
+  });
+}
+creatTask();
+
+function removeTasks() {
+  removeTask.addEventListener('click', () => {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
     }
-
-    )}
+  });
 }
-changeColor()
+
+removeTasks();
+
+function removeDone() {
+  btnRemove.addEventListener('click', () => {
+    while (done.length > 0) {
+      done[0].remove();
+    }
+  });
+}
+removeDone();
