c5fd8a05c5db7295541cfd24231e2fca4963e55f
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-08-31 09:41:58 -0300

    limpa o conteudo do input apos adicionar tarefa

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const taskList = document.querySelector('#lista-tarefas');
-
+// Função que adiciona tarefa a lista
 function adicionaTarefa() {
   const task = document.getElementById('texto-tarefa').value;
   const taskItem = document.createElement('li');
   taskItem.innerHTML = task;
   taskList.appendChild(taskItem);
+
+  // Limpa o conteúdo
+  const taskInput = document.getElementById('texto-tarefa');
+  taskInput.value = '';
 }
 
 const createBtn = document.getElementById('criar-tarefa');
