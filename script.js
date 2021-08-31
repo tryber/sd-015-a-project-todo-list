28ff1269d4124c3079165b2af88b4d0bdef2a912
Jonathan Plets <jonathanplets@gmail.com>
2021-08-31 13:29:53 -0300

    questao 10

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function botaos(nomeBotao) {
   const botao = document.createElement('button');
   botao.id = 'criar-tarefa';
   botao.innerHTML = nomeBotao;
-  const div = document.querySelector('#lista-tarefas');
-  div.appendChild(botao);
+  const body = document.getElementsByTagName('body');
+  body[0].appendChild(botao);
+  console.log(body[0]);
 
 }
 botaos('Criar tarefas');
@@ -20,7 +21,7 @@ function criarTarefa() {
   for (let cont = 0; cont < tagDaLi.length; cont++){
   tagDaLi[cont].addEventListener("click", mudarAcorDoElemento);
   tagDaLi[cont].addEventListener('dblclick', riscarElement);
-
+  
 }
 }
 
@@ -44,18 +45,35 @@ function mudarAcorDoElemento(event) {
 const riscarElement = (event) => {
   console.log(event.target.style.textDecoration);
   
-
-  if(event.target.className == 'completed'){
+  if(event.target.className == 'completed') {
     event.target.className = ' ';
   }else{
     event.target.className = 'completed';
   }
 
-
-
-
-
 }
 
 
+const criarBotao = (nomeDoBotao) => {
+ const botaoApagaTudo = document.createElement('button');
+ botaoApagaTudo.id = 'apaga-tudo';
+ botaoApagaTudo.innerHTML = nomeDoBotao;
+ const body = document.getElementsByTagName('body');
+ body[0].appendChild(botaoApagaTudo);
+ botaoApagaTudo.addEventListener('click', limparItens)
+}
+ 
+const limparItens = () => {
+  const elementoOl = document.querySelector('#lista-tarefas');
+  // const tagDaLi = document.getElementsByTagName('li');
+  elementoOl.innerHTML = ' ';
+//    for(let cont = 0 ; cont < tagDaLi.length ; cont ++){
+//     elementoOl.removeChild(tagDaLi[cont]);
 
+//  }
+
+}
+ 
+ criarBotao('Limpar');
+
+ 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,1 @@
-const tagDaLi = document.getElementsByTagName('li');
-console.log(tagDaLi);
-for(let cont = 0 ; cont < tagDaLi.length ; cont++){
-tagDaLi[cont].addEventListener ('click', mudarAcorDoElemento );
-
-}
-
-function mudarAcorDoElemento() {
-console.log("função")
-
-}
-
+for(let cont = 0; cont < UmElementoPorVez.length ; cont ++) {
