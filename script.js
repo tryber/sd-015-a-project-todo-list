2979c087f600374b4c1d81f103239091742469aa
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-08-31 10:36:00 -0300

    adiciona requisito 9 riscando itens comcluidos

--- script.js
+++ script.js
@@ -18,6 +18,23 @@ function alteraCorOnClick() {
   }
 }
 
+// Função que faz o item ser riscado quando completo
+function riscaTarget(item) {
+  const taskItem = item.target;
+  if (taskItem.className === 'completed') {
+    taskItem.removeAtributte('class');
+  } else {
+    taskItem.classList.add('completed');
+  }
+}
+
+function riscaTargetOnClick() {
+  const taskTarget = document.querySelectorAll('li');
+  for (let i = 0; i < taskTarget.length; i += 1) {
+    taskTarget[i].addEventListener('dblclick', riscaTarget);
+  }
+}
+
 // Função que adiciona tarefa a lista
 function adicionaTarefa() {
   const task = document.getElementById('texto-tarefa').value;
@@ -29,6 +46,7 @@ function adicionaTarefa() {
   const taskInput = document.getElementById('texto-tarefa');
   taskInput.value = '';
   alteraCorOnClick();
+  riscaTargetOnClick();
 }
 
 const createBtn = document.getElementById('criar-tarefa');
