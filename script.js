ffe3ff27c1482b285fc8f693dfe73e5d4c7cc4ef
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-09-01 01:22:13 -0300

    requisitos 11

--- script.js
+++ script.js
@@ -40,8 +40,10 @@ function markDone(clickEvent) {
   }
 }
 
+// Escutador de evento para marcar tarefas concluídas
 clickSelection.addEventListener('dblclick', markDone);
 
+// Função para limpar a lista inteira
 function clearList() {
   const list = document.querySelectorAll('.list-itens');
   for (let i = 0; i < list.length; i += 1) {
@@ -49,5 +51,18 @@ function clearList() {
   }
 }
 
+// Escutador de evento para limpar a lista inteira
 const clearListButton = document.getElementById('apaga-tudo');
 clearListButton.addEventListener('click', clearList);
+
+// Função para limpar tarefas concluídas
+function clearDone() {
+  const list = document.querySelectorAll('.completed');
+  for (let i = 0; i < list.length; i += 1) {
+    list[i].remove();
+  }
+}
+
+// Escutador de evento para limpar tarefas concluídas
+const clearDoneButton = document.getElementById('remover-finalizados');
+clearDoneButton.addEventListener('click', clearDone);
