ec8735dc3fe873c97b8c2b8dfa2a700345f473d5
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-08-31 23:50:21 -0300

    requisitos 6, 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+// função para adicionar tarefas a lista
 function addTaskList() {
   const textItem = document.getElementById('texto-tarefa').value;
-  const tasks = document.getElementById('lista-tarefas');
-  const listItem = document.createElement('li');
-  listItem.innerText = textItem;
-  tasks.appendChild(listItem);
-  document.getElementById('texto-tarefa').value = '';
+  if (textItem !== '') {
+    const tasks = document.getElementById('lista-tarefas');
+    const listItem = document.createElement('li');
+    listItem.innerText = textItem;
+    listItem.className = 'listItens';
+    tasks.appendChild(listItem);
+    document.getElementById('texto-tarefa').value = '';
+  }
 }
 
+// Escutador de evento para o botão de adicionar elementos a lista.
 const clickAddItem = document.getElementById('criar-tarefa');
 clickAddItem.addEventListener('click', addTaskList);
+
+function selectElement(clickEvent) {
+  const taskList = document.querySelectorAll('.listItens');
+  const itenSelection = clickEvent.target;
+  for (let i = 0; i < taskList.length; i += 1) {
+    if (taskList[i].id === 'selected') {
+      taskList[i].removeAttribute('id');
+    }
+  }
+  itenSelection.id = 'selected';
+}
+
+const clickSelection = document.getElementById('lista-tarefas');
+clickSelection.addEventListener('click', selectElement);
