cc7a7eeb0968b95d915a819ca16a43358000d1bb
Anderson-Alencar <anderson23alencar@gmail.com>
2022-03-05 11:52:11 -0300

    prepara para repositorio pessoal

--- script.js
+++ script.js
@@ -2,66 +2,65 @@ const buttonAdd = document.getElementById('criar-tarefa');
 const inputItem = document.getElementById('texto-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
 
-//Cria tarefas
-buttonAdd.addEventListener('click', addTask);
+// Cria tarefas
 
 function addTask() {
-  let item = document.createElement('li');
+  const item = document.createElement('li');
   item.innerText = inputItem.value;
   listTasks.appendChild(item);
   inputItem.value = '';
 }
+buttonAdd.addEventListener('click', addTask);
 
-//Adiciona cor no item clicado
-listTasks.addEventListener('click', paintTask);
+// Adiciona cor no item clicado
 
-function paintTask(event) {
+function paintTask({ target }) {
   for (let i = 0; i < listTasks.children.length; i += 1) {
     listTasks.children[i].style.backgroundColor = 'white';
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    target.style.backgroundColor = 'rgb(188, 188, 188)';
   }
 }
+listTasks.addEventListener('click', paintTask);
 
-//Adiciona taxado com o duplo click
+// Adiciona taxado com o duplo click
+
+function riskTask(event) {
+  event.target.classList.toggle('completed');
+}
 listTasks.addEventListener('dblclick', riskTask);
 
-function riskTask (event) {   
-    event.target.classList.toggle('completed');
-}
-
-//Limpar todas tarefas
+// Limpar todas tarefas
 const buttonDelete = document.getElementById('apaga-tudo');
-buttonDelete.addEventListener('click', clearList);
 
 function clearList() {
-  while(listTasks.firstChild) {
+  while (listTasks.firstChild) {
     listTasks.removeChild(listTasks.lastChild);
-  }  
+  }
 }
+buttonDelete.addEventListener('click', clearList);
 
-//Limpar tarefas completas
+// Limpar tarefas completas
 const btnRemoveTasksCompleted = document.getElementById('remover-finalizados');
-btnRemoveTasksCompleted.addEventListener('click', removeTasksCompleted);
 
 function removeTasksCompleted() {
   for (let i = listTasks.children.length - 1; i >= 0; i -= 1) {
-    if (listTasks.children[i].classList.contains('completed')){
+    if (listTasks.children[i].classList.contains('completed')) {
       listTasks.children[i].remove();
     }
   }
 }
+btnRemoveTasksCompleted.addEventListener('click', removeTasksCompleted);
 
-//Salvar tarefas
+// Salvar tarefas
 const btnSave = document.getElementById('salvar-tarefas');
-btnSave.addEventListener('click', saveTasks);
 
 function saveTasks() {
   localStorage.setItem('tasksSaved', JSON.stringify(listTasks.innerHTML));
 }
+btnSave.addEventListener('click', saveTasks);
 
-let savedList = JSON.parse(localStorage.getItem('tasksSaved'));
+const savedList = JSON.parse(localStorage.getItem('tasksSaved'));
 
 window.onload = function () {
   listTasks.innerHTML = savedList;
-}
-
+};
